293ad26d2ed6481671e339ecdc4d0cad9a8f1f7c
Mangeshsalve <mangeshsalve309994@gmail.com>
2026-03-04 21:55:12 +0530

    prod url added in security config

--- src/main/java/com/pandit/project/config/SecurityConfig.java
+++ src/main/java/com/pandit/project/config/SecurityConfig.java
@@ -54,8 +54,12 @@ public class SecurityConfig {
 	@Bean
 	 public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration config=new CorsConfiguration();
-		config.setAllowedOrigins(List.of(                "http://127.0.0.1:5173",
-                "http://localhost:5173"));
+		config.setAllowedOrigins(List.of(   
+				"http://127.0.0.1:5173",
+                "http://localhost:5173",
+                "https://pandit-front-end.vercel.app",
+                "https://panditprojects.onrender.com"
+				));
 		// TODO Auto-generated method stub
 		config.setAllowedHeaders(List.of("Authorization","Content-Type"));
 		 config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
